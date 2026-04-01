e232f63f9a2c626f9c035c50f232bb5cd5ffd5f5
Shanmukha-Sasidhar <shanmukhrocks09@gmail.com>
2026-04-01 15:34:15 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="shanmukh",
+    author_email="[EMAIL_ADDRESS]",
+    packages=find_packages(),
+    install_requires=[
+       
+    ],
+)
